08853b2e6b627848b89da54e0ce2416bc9a17187
carlosdere23 <carlosdere23@outlook.com>
2025-03-05 20:59:03 -0800

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ import { Server } from 'socket.io';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// Set up Express and Socket.io
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-app.use(express.static(path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')));
+// ...
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 const server = http.createServer(app);
 const io = new Server(server);
