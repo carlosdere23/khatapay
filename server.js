d475ddaaec9265e8b28491dc3dd0329024e0cd67
carlosdere23 <carlosdere23@outlook.com>
2025-03-09 20:18:08 -0800

    Update server.js

--- server.js
+++ server.js
@@ -140,6 +140,3 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-// Correct import for lowdb
-import { Low } from 'lowdb';
-import { JSONFile } from 'lowdb/node';
